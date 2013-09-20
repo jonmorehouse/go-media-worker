012817f5ebf52f00091b8b32d1cbfdd04fdc5986
Jon Morehouse <morehousej09@gmail.com>
2013-09-19 21:34:08 -0700

    Update initial tests to validate strings in go tests

--- src/utilities/file_utilities.go
+++ src/utilities/file_utilities.go
@@ -2,7 +2,7 @@ package utilities
 
 import "code.google.com/p/go-uuid/uuid"
 
-func GenerateS3Keys() string  {
+func GenerateS3Key() string  {
 
 	return uuid.New()
 
--- src/utilities/file_utilities_test.go
+++ src/utilities/file_utilities_test.go
@@ -7,32 +7,45 @@ import (
 	. "utilities"
 	"testing"
 	"fmt"
+	"reflect"
 )
 
 // hook up gocheck to use the go test runner
 func Test(t * testing.T) { TestingT(t) }
 
+// now initialize a testSuite structure that will be passed into various method calls etc
+type TestSuite struct {}
+var _ = Suite(&TestSuite{})
+
+var testInt int = 22
+
 // test that keys are generated properly
-func TestGenerateS3Keys(t *testing.T) {
+func (s *TestSuite) TestGenerateS3Keys(c *C) {
 	
+	// generate a key to evaluate on
+	key := GenerateS3Key()
+	
+	// grab the key reflection value through its interface
+	keyType := reflect.TypeOf(key)
 
-
+	// now make sure that the key type is a string
+	c.Assert(keyType.Kind(), Equals, reflect.String)
 }
 
-// generate s3 key
-func (s *S) TestGenerateS3KeysAreUnique(t *testing.T) {
+//// generate s3 key
+//func (s *S) TestGenerateS3KeysAreUnique(t *testing.T) {
 
-	var key1, key2 string
+	//var key1, key2 string
 
-	// generate 
-	key1 = GenerateS3Keys()
-	key2 = GenerateS3Keys()
+	//// generate 
+	//key1 = GenerateS3Keys()
+	//key2 = GenerateS3Keys()
 	
-	// now lets make sure the two unique keys were 
-	t.Assert(key1, Not(Equals), key2)
+	//// now lets make sure the two unique keys were 
+	//t.Assert(key1, Not(Equals), key2)
 
 		
-}
+//}
 
 
 
