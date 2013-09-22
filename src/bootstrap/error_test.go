87874a149e1d49b48095da905245532d928c8a32
Jon Morehouse <morehousej09@gmail.com>
2013-09-22 13:29:31 -0700

    Initailize error tests for bootstrap package

--- src/bootstrap/error_test.go
+++ src/bootstrap/error_test.go
@@ -4,11 +4,25 @@ import (
 	
 	. "launchpad.net/gocheck"
 	. "bootstrap"
-	"fmt"
+	"reflect"
 )
 
-func (s *TestSuite) TestFatalErrorMessage(c *C) {
+func (s *TestSuite) TestErrorMessage(c *C) {
+	
+	// make sure that we have introduced the correct functions into the current namespace
+	c.Assert(reflect.TypeOf(ErrorMessage).Kind(), Equals, reflect.Func)
+}
 
-	fmt.Println("TESTING 123")	
-	ErrorMessage("TEST")
+// test fatal errors and ensure that we are introducing the correct functions into the namespace
+func (s *TestSuite) TestFatalError(c *C) {
+
+	c.Assert(reflect.TypeOf(FatalError).Kind(), Equals, reflect.Func)
+
+}
+
+// this will be a complicated, rpc-wrapping error in the future etc
+func (s *TestSuite) TestSendError(c *C) {
+
+	c.Assert(reflect.TypeOf(SendError).Kind(), Equals, reflect.Func)
+
 }
