25763be65742f32fbcfaefca977c59fcd8a15d07
Jon Morehouse <morehousej09@gmail.com>
2013-09-22 00:36:42 -0700

    Update gearman application etc

--- src/bootstrap/gearman.go
+++ src/bootstrap/gearman.go
@@ -3,21 +3,56 @@ package bootstrap
 import (
 
 	"github.com/mikespook/gearman-go/worker"	
-	"fmt"
+	//"github.com/mikespook/gearman-go/client"	
+	//"fmt"
+	"log"
 )
 
-func Worker() {
+// this can be created multiple times 
+func CreateWorker() * worker.Worker {
 
-	gearmanWorker := worker.New(worker.Unlimited)
+	// make sure application is bootstrapped and config is ready
+	Bootstrap()
+	
+	// now lets grab all of the hosts that we need for this application
+	hosts, err := Config.Get("gearmanHosts").Array()
+	
+	// handler gearman error as it comes about
+	if err != nil {
 
-	gearmanWorker.Work()	
+		log.Println("Invalid configuration for gearmanHosts")
+	}
+
+	// register a new gearman worker
+	w := worker.New(worker.Unlimited)
+
+	// now loop through and add each host
+	for _, host := range hosts {
+	
+		// assert that the host is a string and add it to the gearmanlist 
+		if str, ok := host.(string); ok {
+
+			// assert that we pass in a string from the element
+			w.AddServer(str)
+		}
+	}
+
+	return w
 }
 
-func Client() {
-
-	fmt.Println("HELLO WORLD")	
+// can create multiple clients for this particular application
+//func CreateClient() *client.Client {
+	
+	//// ensure that application is booted up and ready to go
 	//Bootstrap()
 
-}
+	//// initialize first host
+	//host = Config.Get("gearmanHosts").Array()[0]
+
+	//fmt.Println(host)
+	//// now initialize a client
+	//c := client.New("127.0.0.1:4730")
+	
+//}
 
 
--- src/bootstrap/gearman_test.go
+++ src/bootstrap/gearman_test.go
@@ -3,13 +3,33 @@ package bootstrap_test
 import (
 	
 	. "launchpad.net/gocheck"
+	. "bootstrap"
 )
 
-// 
-func (s *TestSuite) TestGearman(c *C) {
+// lets make sure that we load the client properly etc
+func (s *TestSuite) TestGearmanWorker(c *C) {
+	
+	worker := CreateWorker()
 
+	// make sure that the worker we created is valid
+	c.Assert(worker, Not(Equals), nil)
+
+	// now make sure that a function exists on this object
+	c.Assert(worker.Work, Not(Equals), nil) 
+}
+
+// initialize and ensure that we are properly running this 
+func (s *TestSuite) TestGearmanClient(c *C) {
+
+	// initialize client
+	//client := CreateClient()
+
+	// assert that we actually have created a non-nil pointer etc
+	//c.Assert(client, Not(Equals), nil)
 
 }
+
+
 
 
 
