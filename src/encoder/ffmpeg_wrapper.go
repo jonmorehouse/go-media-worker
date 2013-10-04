fbcc8904efd7b90275183b1c9f0d0b95387af4fe
Jon Morehouse <morehousej09@gmail.com>
2013-10-04 14:36:07 -0700

    Update ffmpeg wrapper structure. Build out goroutine message passing and tests for RunCommand elements

--- src/encoder/ffmpeg_wrapper.go
+++ src/encoder/ffmpeg_wrapper.go
@@ -2,18 +2,62 @@ package encoder
 
 import (
 
-	"fmt"
+	//"fmt"
+	//"os/exec"
+	//"bytes"
 )
 
-func audioOnly(job *Job) {
+// return a channel which will return the status
+func RunCommand(arguments []string) (chan bool, chan string) {
 
-	// generate the command
+	// initialize a signal channel. This will be triggered if there is an error etc
+	signal := make(chan bool, 1)
 
+	// now lets make our string channel to send the output
+	output := make(chan string)
+
+	// this is the go function that is going to run the subcommand and will handle the error checking etc
+	go func() {
+	
+		// run the command and parse results. We need to notify the element of error etc		
+		// signalling false is not good!
+		signal <- false
+		
+	
+
+	}() 
+
+	// now return the various channels here etc
+	return signal, output
+}
+
+func ExtractAudio(job *Job) {
+
+/*
+	// generate the command and then call it on the command line
+	app := "ffmpeg"
+	outputPath := ""
+
+	// now lets actually run this element etc	
+	cmd := exec.Command(app, outputPath) 
+
+	// initialize output elements etc
+	output, err := cmd.Output()
+
+	// initialize error etc
+	if err != nil {
+		
+		//fmt.println(err.Error())
+		//fmt.Println(err.Error())
+		return
+	}
+
+	//fmt.Println(string(output))
+*/
 
 }
 
 func encodeVideo(job *Job) {
-
 
 
 }
--- src/encoder/ffmpeg_wrapper_test.go
+++ src/encoder/ffmpeg_wrapper_test.go
@@ -3,12 +3,71 @@ package encoder_test
 import (
 
 	. "launchpad.net/gocheck"
-	//. "encoder"
+	. "encoder"
+	"sync"
 	"fmt"
 )
 
+// test out the run command element
+func (s *TestSuite) TestExtractAudio(c *C) {
+
+	//RunCommand([]string{"ls", "~/Documents"})
+	ExtractAudio(SampleJob)
+
+}
+
+func (s *TestSuite) TestRunCommand(c *C) {
+	
+	// first set up a wait group for running etc
+	var waitGroup sync.WaitGroup
+
+	// add an element to the wait group
+	waitGroup.Add(1)
+
+	// now lets get the result channel etc
+	signal, output := RunCommand([]string{"ls", "~/Documents"})
+
+	// wait for the timeout
+	select {
+
+		// now listen for the signal to come in
+		case ok, _ := <- signal:
+
+			if ok {
+
+				fmt.Println("Good")
+
+			} else {
+
+				fmt.Println("BAD")
+
+			}
+
+			waitGroup.Done()
+			break
+
+		case value, ok := <- output:
+
+			if !ok {
+				
+				// handle error here 
+			}
+
+			fmt.Println(value)
+
+			waitGroup.Done()
+			break
+
+	}
+
+	// initialize element
+	waitGroup.Wait()
+
+
+}
+
 func (s *TestSuite) TestAudioOnly(c *C) {
-	
-	
+
+	//ExtractAudio(SampleJob)	
 
 }
